c2abcdfdd4a4e6a31feab8044d3126b2e67d092d
Kebechet <samosidor@gmail.com>
2022-10-16 17:45:53 +0200

    added empty API project

---
+++ ApiTest/Controllers/WeatherForecastController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {    }
+}
